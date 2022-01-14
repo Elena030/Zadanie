da2aafab329bc347e9433fd4d8023d2f7161c5e9
Elena030 <lena.vaynberger@yandex.ru>
2022-01-14 11:59:36 +0700

    ok

---
+++ Zadanie 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chapter2.Task4;
+
+namespace Zadanie_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Chapter2.Task4.Program.run();
+        }
+    }
+}
