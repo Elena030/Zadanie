67779f283410cd0745b3e552e8460681d554e916
Elena030 <lena.vaynberger@yandex.ru>
2022-01-14 11:50:19 +0700

    ok

---
+++ Zadanie/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chapter1.Task8;
+
+namespace Zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chapter1.Task8.InterfaceClass.Main();
+        }
+    }
+}
